e8d663d6ff459b34855330ade454fda00fa93403
rafaelferreira_deway <rafael.ferreira@deway.com.br>
2020-01-07 16:03:06 -0300

    :sparkles: finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import  CheckBox  from './CheckBox'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fruites: [
+        {id: 1, value: "banana", isChecked: false},
+        {id: 2, value: "apple", isChecked: false},
+        {id: 3, value: "mango", isChecked: false},
+        {id: 4, value: "grap", isChecked: false}
+      ]
+    }
+  }
+  
+  handleAllChecked = (event) => {
+    let fruites = this.state.fruites
+    fruites.forEach(fruite => fruite.isChecked = event.target.checked) 
+    this.setState({fruites: fruites})
+  }
+
+  handleCheckChieldElement = (event) => {
+    let fruites = this.state.fruites
+    fruites.forEach(fruite => {
+      console.log("handleCheckChieldElement | fruite: ", fruite);
+      console.log("handleCheckChieldElement | fruite.value: ", fruite.value);
+      console.log("handleCheckChieldElement | event.target.value: ", event.target.value);
+       if (fruite.value === event.target.value)
+          fruite.isChecked = event.target.checked
+    })
+    this.setState({fruites: fruites})
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1> Check and Uncheck All Example </h1>
+      <input type="checkbox" onClick={this.handleAllChecked}  value="checkedall" /> Check / Uncheck All
+        <ul>
+        {
+          this.state.fruites.map((fruite) => {
+            return (<CheckBox handleCheckChieldElement={this.handleCheckChieldElement}  {...fruite} />)
+          })
+        }
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
